7bc9e7ae3cbd8eccfa6b5931d1ab525ddd2a8da2
NGOCDE <97270021+NGOCDE@users.noreply.github.com>
2023-07-27 13:31:03 +0700

    test prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -1,32 +1,51 @@
-const express = require('express')
-const path = require('path')
-const morgan = require('morgan')
-const exphbs = require('express-handlebars')
-const app = express()
-const port = 3000
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const app = express();
+const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+
+app.use(express.json());
 
 // http logger
-app.use(morgan('combined'))
+//app.use(morgan('combined'))
 
 //template engine
-app.engine('hbs', exphbs.engine({
-  extname: '.hbs'
-}))
+app.engine(
+    'hbs',
+    exphbs.engine({
+        extname: '.hbs',
+    }),
+);
 
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'resources/views'))
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
+app.get('/', (req, res) => {
+    res.render('home');
+});
 
-app.get('/', (req, res) => { 
-  res.render('home')
-})
+app.get('/news', (req, res) => {
+    res.render('news');
+});
 
-app.get('/news', (req, res) => { 
-  res.render('news')
-})
+app.get('/search', (req, res) => {
+    res.render('search');
+});
+
+        app.post('/search', (req, res) => {
+        console.log(req.body);
+        res.send('search');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
